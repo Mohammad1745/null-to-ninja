cb0833acb0c5f2ae2f4d95b7df782b5abc1f0083
Mohammad1745 <mdali2016.227@gmail.com>
2023-03-03 11:58:56 +0600

    [Add] task api

---
+++ blog-api/config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_origins' => ['http://127.0.0.1:5001'],
+    'allowed_origins_patterns' => ['*'],
+    'allowed_headers' => ['Content-Type', '*'],
+    'exposed_headers' => ['*'],
+    'max_age' => 10,
+    'supports_credentials' => true,
+];
